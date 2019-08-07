ce2ab9d61221cc873210f0dff0b2fb2b4a46098a
jason <jasonedlewis@gmail.com>
2019-08-07 10:31:11 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+    get '/' do 
+        
+        erb :index
+    end
+
+    post '/' do
+        @string = params[:string]
+
+        erb :
+    end
+
 end
